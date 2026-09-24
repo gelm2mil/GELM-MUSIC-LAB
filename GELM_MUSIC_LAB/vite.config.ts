e25b95d7dd3124410d9aa64b67765145f46c4665
gelm2mil <nerox2mil@proton.me>
2026-09-23 22:30:32 -0600

    Add base URL to Vite configuration
    Set base URL for the project in Vite configuration.

--- GELM_MUSIC_LAB/vite.config.ts
+++ GELM_MUSIC_LAB/vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/GELM-MUSIC-LAB/",
   plugins: [react()],
   optimizeDeps: {
     exclude: [
